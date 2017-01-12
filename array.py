0c0c0f7cfe62f76cd3df03f34eeff1bd8cb85b7e
zhangjun <zhangjun@ehousechina.com>
2017-01-12 14:44:11 +0800

    first

---
+++ array.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+
+
+
+def function():
+	arrays=["xxx","hehehe","yoyo"]
+	print len(arrays)
+	for x in arrays:
+		print x
+		pass
+	
+
+function()
+		
---
+++ input.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+
+
+def function1(name):
+	print 'xxx %s' %name
+
+def function2():
+	
+	pass
+
+		
+
+function1(666)
+
+name = raw_input()
+
+function1(name)
